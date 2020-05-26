b0291740eaf0e3ed0ebd217fc60b1431ff88cf3c
Vanderci <vandercijr@gmail.com>
2020-05-25 22:03:32 -0300

    add derivative routes

--- src/routes/derivative.js
+++ src/routes/derivative.js
@@ -3,7 +3,10 @@
 const express = require('express');
 const router  = express.Router();
 const DerivativeController  = require('../controllers/derivative.controller.js');
+const StockNegotiationController  = require('../controllers/stock.negotiation.controller.js');
 
+router.get('/negotiation/:mkt_type/:stock_prefix*?', StockNegotiationController.getAll);
+router.get('/negotiation/last', StockNegotiationController.getNegotiationDate);
 router.get('/:symbol', DerivativeController.getOne);
 
 module.exports = router;
