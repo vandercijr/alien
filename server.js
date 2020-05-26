4126bdb7c5fafb94168b145e7faad7612a08c7fe
Vanderci <vandercijr@gmail.com>
2020-05-25 21:53:52 -0300

    add routes and last negotiation date

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
 'use strict'
 
 const bodyParser  = require('body-parser');
-const app     = require('express')();
+const express     = require('express');
+const app         = express();
 const path    = require("path");
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 3000;
 const derivativeRoutes = require('./src/routes/derivative');
+const StockNegotiationService = require('./src/services/stock.negotiation.service');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use('/derivative', derivativeRoutes);
 
-module.exports = app.listen(port, () => {
+
+StockNegotiationService.getNegotiationDate()
+.then((res) =>  {
+  app.set('stock_negotiation_date', res[0].stock_negotiation_date);
+});
+
+// .then((res) =>  {
+//   app.set('stock_negotiation_date', res[0].stock_negotiation_date)
+// })
+
+app.listen(port, () => {
   console.log('Alien server is alive on port', port);
 });
+
+module.exports = app;
